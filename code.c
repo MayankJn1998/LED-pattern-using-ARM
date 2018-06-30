260e306529af02cc5f62ca73fbc9ef4983e9103e
mayank jain <mayankjain.shanu@gmail.com>
2018-06-30 22:13:28 +0530

    functions done

--- code.c
+++ code.c
@@ -30,15 +30,78 @@ void switch_1(){
 	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,GPIO_PIN_SET);
 	delay(500);
 	
+	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,GPIO_PIN_RESET);
+	delay(500);
+	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,GPIO_PIN_RESET);
+	delay(500);
+	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_13,GPIO_PIN_RESET);
+	delay(500);
+	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_12,GPIO_PIN_RESET);
+	delay(500);	
+	
+}
+
+void set1010(){
+	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_12,GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_13,GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,GPIO_PIN_RESET);
+}
+
+void set1001(){
+	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_12,GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_13,GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,GPIO_PIN_SET);
+}
+
+void setOn(){
+	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_12,GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_13,GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,GPIO_PIN_SET);
+}
+
+void toggle(){
+		set1010();
+		delay(500);
+		HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_12);
+		HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_13);
+		HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_14);
+		HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_15);
+		delay(500);
+}
+
+void switch_3(){
+
+	
+
 }
 
 int main(){
 
 		config();
+		int count=1; 
 		while(1){
 			
 			if(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_0) == GPIO_PIN_SET){
-				
+					int temp=20000;
+					setOn();
+					while(temp>=0){
+						if(HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_0) == GPIO_PIN_SET){
+							count++;
+							if(count>=3) break;
+						}
+					}
+			}
+			
+			switch(count){
+				case 1:	switch_1();
+								break;
+				case 2:	toggle();
+								break;
+				case 3:	
+								break;
 			}
 			
 		}
